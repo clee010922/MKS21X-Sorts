e77b212cbf8d4017ac6484e44932ccbcb58be765
clee010922 <clee01@stuy.edu>
2018-12-05 09:51:32 -0500

    changed the class name to Sorts

---
+++ Sorts.java
@@ -0,0 +1,42 @@
+public class Sorts {
+
+
+  public static void selectionSort(int[] ary) {
+    int min = ary[0];
+    int index = 0;
+
+    for (int i = 0; i < ary.length; i++) {
+      min = ary[i];
+      index = i;
+      for (int x = i; x < ary.length; x++) {
+        if (ary[x] < min) {
+          min = ary[x];
+          index = x;
+        }
+      }
+      ary[index] = ary[i];
+      ary[i] = min;
+    }
+  }
+
+  public static String printArray(int[] ary) {
+    String result = "[";
+    for (int i = 0; i < ary.length; i++) {
+      result += ary[i];
+      if (i != ary.length - 1)
+        result += ", ";
+    }
+    result += "]";
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int[] test = {4, 3, 2, 1, 4, 3, 2, 1};
+    int[] test2 = {-1, 50, 2, 900000, -99999, 50, 0};
+    selectionSort(test);
+    selectionSort(test2);
+    System.out.println(printArray(test));
+    System.out.println(printArray(test2));
+  }
+
+}
