f85e1b36ecda63143957734869ab5d7d22320388
clee010922 <clee01@stuy.edu>
2018-12-04 22:37:54 -0500

    added main method to test the sort method. verified that it works

--- SelectionSort.java
+++ SelectionSort.java
@@ -30,5 +30,13 @@ public class SelectionSort {
     return result;
   }
 
+  public static void main(String[] args) {
+    int[] test = {4, 3, 2, 1, 4, 3, 2, 1};
+    int[] test2 = {-1, 50, 2, 900000, -99999, 50, 0};
+    selectionSort(test);
+    selectionSort(test2);
+    System.out.println(printArray(test));
+    System.out.println(printArray(test2));
+  }
 
 }
